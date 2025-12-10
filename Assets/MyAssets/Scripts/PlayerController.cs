72106bc39a31e1612d401282a03b99b3a8b838b8
canuto31 <jcmt.personal.vg@gmail.com>
2025-12-10 18:26:53 -0500

    Add jump logic

--- Assets/MyAssets/Scripts/PlayerController.cs
+++ Assets/MyAssets/Scripts/PlayerController.cs
@@ -5,16 +5,28 @@ public class PlayerController : MonoBehaviour
 {
     //-----Components-----
     public Rigidbody rb;
+
     //-----Player Movement-----
     private Vector2 _inputVector;
-    
-    public float health;
+
     public float speed;
+
+    //-----Player Jump-----
+    public float jumpForce;
+    public Transform groundCheck;
+    public float groundRadius;
+    public LayerMask groundLayer;
+
+    private bool _isGrounded;
+
+    public float health;
 
     void FixedUpdate()
     {
         Vector3 moveDirection = default;
         MovePlayer(moveDirection);
+
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundLayer);
     }
 
     private void MovePlayer(Vector3 moveDirection)
@@ -29,5 +41,24 @@ public class PlayerController : MonoBehaviour
         {
             _inputVector = context.ReadValue<Vector2>();
         }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (_isGrounded)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
     }
 }
