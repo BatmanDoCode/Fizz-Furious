b3c9e56f25a28ffdd82a96c9fa9d46829c11e728
canuto31 <jcmt.personal.vg@gmail.com>
2025-12-10 20:24:55 -0500

    Add logic to change material color when the player hit the enemy

--- Assets/MyAssets/Scripts/EnemyController.cs
+++ Assets/MyAssets/Scripts/EnemyController.cs
@@ -1,17 +1,36 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public float health;
 
+    private MeshRenderer _meshRenderer;
+    private Color _originalColor;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _originalColor = _meshRenderer.material.color;
+    }
+
     public void TakeDamage(float damage)
     {
         health -= damage;
+        StartCoroutine(FlashDamage());
 
         if (health <= 0)
         {
             Die();
         }
+    }
+    
+    private IEnumerator FlashDamage()
+    {
+        _meshRenderer.material.color = Color.white;
+        yield return new WaitForSeconds(0.1f);
+        _meshRenderer.material.color = _originalColor;
     }
 
     private void Die()
